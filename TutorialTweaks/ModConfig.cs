8d3d9112c9abfdf2df09e994de3afbf150a7a292
MadSkunky <94592595+MadSkunky@users.noreply.github.com>
2021-11-18 16:38:42 +0100

    Config reordered

--- TutorialTweaks/ModConfig.cs
+++ TutorialTweaks/ModConfig.cs
@@ -4,21 +4,22 @@
     internal class ModConfig
     {
         // config fields
-        // If true Jacob get the modified 'Ares SAR-1 mod' as main weapon (see below), otherwise a standard Ares AR-1
-        public bool GiveJacobAssaultRifleProficiency = true;
-        public bool GiveJacobNewSpecialAres = true;
+        // If true Jacob get the new modified special Ares as main weapon (see below), otherwise a standard Ares AR-1
+        public bool GiveJacobAssaultRifleProficiency = false;
+        public bool GiveJacobNewSpecialAres = false;
+
         // Modification to Jacobs 'Ares SAR-1 mod'
         public NewSpecialAresSetStruct NewSpecialAresSettings = new NewSpecialAresSetStruct(
-            true,
+            false,
+            false,
+            6,
+            95,
+            65,
             35,
             1,
             0,
             4,
-            40,
-            false,
-            6,
-            95,
-            65
+            40
             );
         // DebugLevel (0: nothing, 1: error, 2: debug, 3: info)
         public int Debug = 1;
@@ -27,35 +28,34 @@
     {
         public NewSpecialAresSetStruct(
             bool nopenalty,
+            bool manufacturable,
+            int tech,
+            int mat,
+            int tp,
             int damage,
             int shred,
             int pierce,
             int burst,
-            int er,
-            bool manufacturable,
-            int tech,
-            int mat,
-            int tp
+            int er
             )
         {
             NoPenaltyWithoutProficiency = nopenalty;
+            Manufacturable = manufacturable;
+            ManufacturingCost = new ManufacureCost(tech, mat, tp);
             Damage = damage;
             Shred = shred;
             Pierce = pierce;
             BurstCount = burst;
             EffectiveRange = er;
-            Manufacturable = manufacturable;
-            ManufacturingCost = new ManufacureCost(tech, mat, tp);
         }
-
         public bool NoPenaltyWithoutProficiency;
+        public bool Manufacturable;
+        public ManufacureCost ManufacturingCost;
         public int Damage;
         public int Shred;
         public int Pierce;
         public int BurstCount;
         public int EffectiveRange;
-        public bool Manufacturable;
-        public ManufacureCost ManufacturingCost;
         internal struct ManufacureCost
         {
             public ManufacureCost(int tech, int materials, int tp) : this()
